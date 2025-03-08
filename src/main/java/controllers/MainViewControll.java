e0f9912579f7d16e3e21cd1b5728d6da64a0cdbb
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-03-08 13:25:34 -0300

    tela inicial

---
+++ src/main/java/controllers/MainViewControll.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainViewControll {
+
+    @FXML
+    private TextField textField_theard;
+
+}

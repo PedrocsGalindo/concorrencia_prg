f639c0f895e3aff602a047f6b2f9cc2313825d85
Pedro Cesar Galindo <pedrocsgalindo@gmail.com>
2025-03-10 08:52:00 -0300

    verificação do textfield

--- src/main/java/controllers/MainViewControll.java
+++ src/main/java/controllers/MainViewControll.java
@@ -9,6 +9,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 import java.io.IOException;
 
@@ -22,22 +24,30 @@ public class MainViewControll {
 
     @FXML
     void click_button_testar(ActionEvent event) {
-        try{
-            // tela de grafico
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/graphicView.fxml"));
-            Parent root = loader.load();
 
-            // janela atula
-            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            stage.setScene(new Scene(root));
-            stage.setTitle("Nova Tela");
-            stage.show();
+        if (textField_theard.getText().isEmpty()){
+            try{
+                // tela de grafico
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/graphicView.fxml"));
+                Parent root = loader.load();
 
-        } catch (IOException e) {
-            System.err.println("Erro ao carregar a nova tela: " + e.getMessage());
-            e.printStackTrace();
+                // janela atula
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setScene(new Scene(root));
+                stage.setTitle("Nova Tela");
+                stage.show();
+
+            } catch (IOException e) {
+                System.err.println("Erro ao carregar a nova tela: " + e.getMessage());
+                e.printStackTrace();
+            }
+        } else {
+            Alert alerta = new Alert(AlertType.WARNING);
+            alerta.setTitle("Aviso");
+            alerta.setHeaderText(null);
+            alerta.setContentText("Você deve preencher o campo com o nuemro de threads");
+            alerta.showAndWait();
         }
-
     }
 }
 
